e6b0a1ed7cec3c9019e8434b59e93b5220fc52c0
ddz <ddztomcat@gmail.com>
2023-11-01 19:45:08 +0800

    fix: notNull

--- lib/android/src/main/java/cn/qiuxiang/react/geolocation/AMapGeolocationPackage.java
+++ lib/android/src/main/java/cn/qiuxiang/react/geolocation/AMapGeolocationPackage.java
@@ -1,24 +1,24 @@
 package cn.qiuxiang.react.geolocation;
 
-import androidx.annotation.NonNull;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class AMapGeolocationPackage implements ReactPackage {
-    @NonNull
     @Override
-    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
-    @NonNull
     @Override
-    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-        return Collections.singletonList(new AMapGeolocationModule(reactContext));
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new AMapGeolocationModule(reactContext));
+        return modules;
     }
 }
