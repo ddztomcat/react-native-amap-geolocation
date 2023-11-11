a72a2eef4f861c4f6032591189013ac680f2c0f3
ddz <ddztomcat@gmail.com>
2023-11-11 11:22:19 +0800

    fix

---
+++ lib/js/index.d.ts
@@ -0,0 +1,5 @@
+export * from "./types";
+export * from "./geolocation";
+export * from "./amap-geolocation";
+export { default as Geolocation } from "./geolocation";
+//# sourceMappingURL=index.d.ts.map
